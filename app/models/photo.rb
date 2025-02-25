8004d47f6f807bb7de5f62b44d15a3343e602e2f
CruzAlanna <lannikcru@gmail.com>
2025-02-25 20:52:36 +1000

    Add photo,post,comments using polymorphic assoc.

---
+++ app/models/photo.rb
@@ -0,0 +1,5 @@
+class Photo < ApplicationRecord
+  has_many :comments, as: :commentable
+  
+  validates :title, :url, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@ Product.create!(name: "Coffee", price: 2.99, category: beverage)
 Product.create!(name: "Lemonade", price: 1.99, category: beverage)
 Product.create!(name: "Chips", price: 1.50, category: snack)
 Product.create!(name: "Pretzels", price: 2.36, category: snack)
+
+
+post = Post.create(title: "First Post", content: "Hello, world.")
+photo = Photo.create(title: "Sunset", url: "http://example.com/sunset.jpg")
+Comment.create(body: "Nice post!", commentable: post)
+Comment.create(body: "Beautiful pic!", commentable: photo)
